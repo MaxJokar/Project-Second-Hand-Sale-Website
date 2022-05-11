7906968bb9eda2c3d841c2d8f74872959e8e163b
email <mmb2020max@gmail.com>
2022-05-11 13:11:26 +0200

    2 Configuration in setting Done

--- sh/sh/settings.py
+++ sh/sh/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.blog',
+    'apps.media',
 ]
 
 MIDDLEWARE = [
@@ -51,10 +54,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'sh.urls'
 
+#for Templates we have DIR:[DIRS': [os.path.join(BASE_DIR,'templates/'),],]
+#but here we have from the first lesson below one , so far use it then 
+#have a look at what happen,if we need we will change it to the
+#'DIRS': [os.path.join(BASE_DIR,'templates/'),],
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR+'/templates/',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,10 +82,21 @@ WSGI_APPLICATION = 'sh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #Original data give below but we change it to the next one :
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'mysql.connector.django',
+        'ENGINE':'django.db.backends.mysql',
+        'NAME': 'dbStockHouse',
+        'USER':'root',
+        'PASSWORD':'123456',
+        'HOST': 'localhost',
+        'PORT':'3306',
     }
-}
+          
+        
+    }
+
 
 
 # Password validation
@@ -115,9 +133,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static/'),
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media/'),
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
